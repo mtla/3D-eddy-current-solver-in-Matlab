b693a55127b718f9769c9141328291f0231f6e63
Henrik Hillner <henrik.hillner@aalto.fi>
2017-02-07 16:38:50 +0200

    Brings back reading normals from obj file

--- read_obj.m
+++ read_obj.m
@@ -51,6 +51,13 @@ file_words = file2cellarray( fullfilename);
 
 % Vertex data
 vertices=[]; nv=0;
+vertices_texture=[]; nvt=0;
+vertices_point=[]; nvp=0;
+vertices_normal=[]; nvn=0;
+material=[];
+
+% Surface data
+no=0;
 
 % Loop through the Wavefront object file
 for iline=1:length(ftype)
@@ -75,14 +82,199 @@ for iline=1:length(ftype)
                 % Add to vertices list X Y Z W
                 vertices(nv,1:4)=data;
             end
+        case('vp')
+            % Specifies a point in the parameter space of curve or surface
+            nvp=nvp+1;
+            if(length(data)==1)
+                % Reserve block of memory
+                if(mod(nvp,10000)==1), vertices_point(nvp+1:nvp+10001,1)=0; end
+                % Add to vertices point list U
+                vertices_point(nvp,1)=data;
+            elseif(length(data)==2)
+                % Reserve block of memory
+                if(mod(nvp,10000)==1), vertices_point(nvp+1:nvp+10001,1:2)=0; end
+                % Add to vertices point list U V
+                vertices_point(nvp,1:2)=data;
+            else
+                % Reserve block of memory
+                if(mod(nvp,10000)==1), vertices_point(nvp+1:nvp+10001,1:3)=0; end
+                % Add to vertices point list U V W
+                vertices_point(nvp,1:3)=data;
+            end
+        case('vn')
+            % A normal vector
+            nvn=nvn+1; if(mod(nvn,10000)==1),  vertices_normal(nvn+1:nvn+10001,1:3)=0; end
+            % Add to vertices list I J K
+            vertices_normal(nvn,1:3)=data;
+        case('vt')
+            % Vertices Texture Coordinate in photo
+            % U V W
+            nvt=nvt+1;
+            if(length(data)==1)
+                % Reserve block of memory
+                if(mod(nvt,10000)==1), vertices_texture(nvt+1:nvt+10001,1)=0; end
+                % Add to vertices texture list U
+                vertices_texture(nvt,1)=data;
+            elseif(length(data)==2)
+                % Reserve block of memory
+                if(mod(nvt,10000)==1), vertices_texture(nvt+1:nvt+10001,1:2)=0; end
+                % Add to vertices texture list U V
+                vertices_texture(nvt,1:2)=data;
+            else
+                % Reserve block of memory
+                if(mod(nvt,10000)==1), vertices_texture(nvt+1:nvt+10001,1:3)=0; end
+                % Add to vertices texture list U V W
+                vertices_texture(nvt,1:3)=data;
+            end
+        case('l')
+            no=no+1; if(mod(no,10000)==1), objects(no+10001).data=0; end
+            array_vertices=[];
+            array_texture=[];
+            for i=1:length(data),
+                switch class(data)
+                    case 'cell'
+                        tvals=str2double(stringsplit(data{i},'/'));
+                    case 'string'
+                        tvals=str2double(stringsplit(data,'/'));
+                    otherwise
+                        tvals=data(i);
+                end
+                val=tvals(1);
+                if(val<0), val=val+1+nv; end
+                array_vertices(i)=val;
+                if(length(tvals)>1),
+                    val=tvals(2);
+                    if(val<0), val=val+1+nvt; end
+                    array_texture(i)=val;
+                end
+            end
+            objects(no).type='l';
+            objects(no).data.vertices=array_vertices;
+            objects(no).data.texture=array_texture;
+        case('f')
+            no=no+1; if(mod(no,10000)==1), objects(no+10001).data=0; end
+            array_vertices=[];
+            array_texture=[];
+            array_normal=[];
+            for i=1:length(data);
+                switch class(data)
+                    case 'cell'
+                        tvals=str2double(stringsplit(data{i},'/'));
+                    case 'string'
+                        tvals=str2double(stringsplit(data,'/'));
+                    otherwise
+                        tvals=data(i);
+                end
+                val=tvals(1);
+                
+                if(val<0), val=val+1+nv; end
+                array_vertices(i)=val;
+                if(length(tvals)>1),
+                    if(isfinite(tvals(2)))
+                        val=tvals(2);
+                        if(val<0), val=val+1+nvt; end
+                        array_texture(i)=val;
+                    end
+                end
+                if(length(tvals)>2),
+                    val=tvals(3);
+                    if(val<0), val=val+1+nvn; end
+                    array_normal(i)=val;
+                end
+            end
+            
+            % A face of more than 3 indices is always split into
+            % multiple faces of only 3 indices.
+            objects(no).type='f';
+            findex=1:min (3,length(array_vertices));
+           
+            objects(no).data.vertices=array_vertices(findex);
+            if(~isempty(array_texture)),objects(no).data.texture=array_texture(findex); end
+            if(~isempty(array_normal)),objects(no).data.normal=array_normal(findex); end
+            for i=1:length(array_vertices)-3;
+                no=no+1; if(mod(no,10000)==1), objects(no+10001).data=0; end
+                findex=[1 2+i 3+i];
+                findex(findex>length(array_vertices))=findex(findex>length(array_vertices))-length(array_vertices);
+                objects(no).type='f';
+                objects(no).data.vertices=array_vertices(findex);
+                if(~isempty(array_texture)),objects(no).data.texture=array_texture(findex); end
+                if(~isempty(array_normal)),objects(no).data.normal=array_normal(findex); end
+            end
+        case{'#','$'}
+            % Comment
+            tline='  %'; 
+            if(iscell(data))
+                for i=1:length(data), tline=[tline ' ' data{i}]; end
+            else
+                tline=[tline data];
+            end
+            if(verbose), disp(tline); end
+        case{''}
+        otherwise
+            no=no+1;
+            if(mod(no,10000)==1), objects(no+10001).data=0; end
+            objects(no).type=type;
+            objects(no).data=data;
     end
 end
 
+% Initialize new object list, which will contain the "collapsed" objects
+objects2(no).data=0;
+
 index=0;
 i=0;
+while (i<no), i=i+1;
+    type=objects(i).type;
+    % First face found
+    if((length(type)==1)&&(type(1)=='f'))
+        % Get number of faces
+        for j=i:no
+            type=objects(j).type;
+            if((length(type)~=1)||(type(1)~='f'))
+                j=j-1; break;
+            end
+        end
+        numfaces=(j-i)+1;
+        
+        index=index+1;
+        objects2(index).type='f';
+        % Process last face first to allocate memory
+        objects2(index).data.vertices(numfaces,:)= objects(i).data.vertices;
+        if(isfield(objects(i).data,'texture'))
+            objects2(index).data.texture(numfaces,:) = objects(i).data.texture;
+        else
+            objects2(index).data.texture=[];
+        end
+        if(isfield(objects(i).data,'normal'))
+            objects2(index).data.normal(numfaces,:)  = objects(i).data.normal;
+        else
+            objects2(index).data.normal=[];
+        end
+        % All faces to arrays
+        for k=1:numfaces
+            objects2(index).data.vertices(k,:)= objects(i+k-1).data.vertices;
+            if(isfield(objects(i).data,'texture'))
+                objects2(index).data.texture(k,:) = objects(i+k-1).data.texture;
+            end
+            if(isfield(objects(i).data,'normal'))
+                objects2(index).data.normal(k,:)  = objects(i+k-1).data.normal;
+            end
+        end
+        i=j;
+    else
+        index=index+1;
+        objects2(index).type=objects(i).type;
+        objects2(index).data=objects(i).data;
+    end
+end
 
 % Add all data to output struct
-OBJ=vertices(1:nv,:);
+OBJ.objects=objects2(1:index);
+OBJ.material=material;
+OBJ.vertices=vertices(1:nv,:);
+OBJ.vertices_point=vertices_point(1:nvp,:);
+OBJ.vertices_normal=vertices_normal(1:nvn,:);
+OBJ.vertices_texture=vertices_texture(1:nvt,:);
 if(verbose),disp('Finished Reading Object file'); end
 
 
