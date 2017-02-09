85911886deb29764729cab71c09b83050f0ad23d
Henrik Hillner <henrik.hillner@aalto.fi>
2017-02-09 13:24:00 +0200

    Adds description to calculate_tetrahedrons

--- calculate_tetrahedrons.m
+++ calculate_tetrahedrons.m
@@ -1,8 +1,22 @@
 function [ output_args ] = calculate_tetrahedrons( input_args )
-%CALCULATE_TETRAHEDRONS 
+% This function converts an array of 3D vertices into
+% the appropriate formulation needed in the FEM calculation
+%
+% input: [m×3 double]
+% output: [m×3 double]
+%
+% example input: 
+%      1     1    -1
+%      1    -1    -1
+%      1     1     1
+%      1    -1     1
+%     -1     1    -1
+%     -1    -1    -1
+%     -1     1     1
+%     -1    -1     1
+%
+% exmaple output:
 % 
-%   Detailed explanation goes here
-
 
 end
 
