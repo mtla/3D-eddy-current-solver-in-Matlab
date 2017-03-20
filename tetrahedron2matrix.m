3d3e3b7c465e3ea210d1f9cd1da52c9b63a03691
Henrik Hillner <henrik.hillner@aalto.fi>
2017-03-20 13:08:59 +0200

    Adds explanations to the tetrahedron2matix function

--- tetrahedron2matrix.m
+++ tetrahedron2matrix.m
@@ -1,14 +1,27 @@
 function [ tetrahedron_matrix ] = tetrahedron2matrix( tetrahedron, nodes_coordinates )
 % TETRAHEDRON2MATRIX This functions takes an tetrahedron, along with a list
 % of the coordinates of the nodes, as an input and outputs an 4x4 matrix
-%   Detailed explanation goes here
+%
+% Input:
+%       tetrahedron: 
+%           array with for elements
+%       nodes_coordinates: 
+%           3xn matrix with coordinates for each vertice in
+%           the mesh
+%
+% Output:
+%       tetrahedron_matrix:
+%           4x4 matrix with values according to the shape function
+%
+% Syntax: tetrahedron2matrix(tetrahedron, nodes_coordinates)
+%
 
     tetrahedron_matrix = zeros(4,4);
     x = zeros(1,4);
     y = zeros(1,4);
     z = zeros(1,4);
     for n = 1:4
-        x(n) = nodes_coordinates(tetrahedron(n),1)
+        x(n) = nodes_coordinates(tetrahedron(n),1);
         y(n) = nodes_coordinates(tetrahedron(n),2);
         z(n) = nodes_coordinates(tetrahedron(n),3);
     end
