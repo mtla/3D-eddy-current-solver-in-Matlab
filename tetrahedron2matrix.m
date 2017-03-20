6473e1b37348dbf616c595aa1575957181e4e0c8
Henrik Hillner <henrik.hillner@aalto.fi>
2017-03-20 12:56:23 +0200

    Makes a better dummy function

--- tetrahedron2matrix.m
+++ tetrahedron2matrix.m
@@ -1,10 +1,27 @@
-function [ tetrahedron_matrix ] = tetrahedron2matrix( tetrahedron, nodes_list )
+function [ tetrahedron_matrix ] = tetrahedron2matrix( tetrahedron, nodes_coordinates )
 % TETRAHEDRON2MATRIX This functions takes an tetrahedron, along with a list
-% of the available nodes, as an input and outputs an 4x4 matrix
+% of the coordinates of the nodes, as an input and outputs an 4x4 matrix
 %   Detailed explanation goes here
 
-    tetrahedron_matrix = zeros(4,4)
-    
-
+    tetrahedron_matrix = zeros(4,4);
+    x = zeros(1,4);
+    y = zeros(1,4);
+    z = zeros(1,4);
+    for n = 1:4
+        x(n) = nodes_coordinates(tetrahedron(n),1)
+        y(n) = nodes_coordinates(tetrahedron(n),2);
+        z(n) = nodes_coordinates(tetrahedron(n),3);
+    end
+    for i = 1:4
+        for j = 1:4
+            % Determines the value of the specific S_ij
+            % with the help of the shapefunction
+            % 
+            % CURRENT SHAPE FUNCTION IS NOT THE CORRECT ONE
+            if (i ~= j)
+                tetrahedron_matrix(i,j) = (x(i)-x(j))+(y(j)-y(i));
+            end
+        end
+    end
 end
 
