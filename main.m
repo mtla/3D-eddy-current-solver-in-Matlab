59d7137a6317bcb051d097a9b10f9129a1701386
Henrik Hillner <henrik.hillner@aalto.fi>
2017-03-31 14:09:08 +0300

    adds test of tetrahedron2matrix function to main

--- main.m
+++ main.m
@@ -2,3 +2,10 @@ read_obj
 verts = ans;
 DT = delaunayTriangulation(verts.vertices);
 tetramesh(DT);
+
+tetrahedrons = DT.ConnectivityList;
+tetrahedron = tetrahedrons(1,:);
+points_list = DT.Points;
+
+% test tetrahedron2matrix
+tetrahedron2matrix(tetrahedron, points_list)
