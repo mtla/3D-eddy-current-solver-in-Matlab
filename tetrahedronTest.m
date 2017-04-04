7a46ca80d9b6d097707b5ba02d8282e293e7c867
Henrik Hillner <henrik.hillner@aalto.fi>
2017-04-04 17:49:21 +0300

    Minor fix to tetrahedron unit test that doesn't actually fix it

--- tetrahedronTest.m
+++ tetrahedronTest.m
@@ -2,15 +2,15 @@ classdef tetrahedronTest < matlab.unittest.TestCase
     %TETRAHEDRONTEST Summary of this class goes here
     %   Detailed explanation goes here
     
-    properties
-        tetrahedron = [1 2 3 4];
-        node_list = [0 0 0, 0 0 1, 0 1 0, 1 0 0];
+    properties (TestParameter)
+        tetrahedron = {1 2 3 4};
+        node_list = {0 0 0, 0 0 1, 0 1 0, 1 0 0};
     end
     
     methods (Test)
         function testDeterminant(testCase)
             actSolution = det(tetrahedron2matrix(tetrahedron, node_list));
-            exSolution = 0;
+            expSolution = 0;
             testCase.verifyEqual(actSolution, expSolution);
         end
     end
