0b9408770f71948e0bf54170e319219a51bf2973
Henrik Hillner <henrik.hillner@aalto.fi>
2017-04-04 17:01:29 +0300

    Adds dummy stiffness matrix build function

---
+++ buildStiffnesMatrix.m
@@ -0,0 +1,8 @@
+function [ sMatrix ] = buildStiffnesMatrix( tetrahedrons, verticesList )
+%BUILDSTIFFNESMATRIX Summary of this function goes here
+%   Detailed explanation goes here
+
+    n = tetrahedrons(1,:)(end);
+    sMatrix = n;
+end
+
