5410fd97d0577bfa79a3e1ba405eb96629199375
Henrik Hillner <henrik.hillner@aalto.fi>
2017-04-07 09:55:33 +0300

    First version of stiffness matrix construction

--- buildStiffnessMatrix.m
+++ buildStiffnessMatrix.m
@@ -2,12 +2,16 @@ function [ sMatrix ] = buildStiffnessMatrix( tetrahedrons, vertices_list )
 %BUILDSTIFFNESMATRIX Summary of this function goes here
 %   Detailed explanation goes here
 
-    sMatrix = zeros(size(tetrahedrons, 1));
-%     for row = 1:size(tetrahedrons, 1)
-%         tetrahedron = tetrahedrons(row, :);
-%         S = tetrahedron2matrix(tetrahedron, vertices_list);
-%     end
-%     n = tetrahedrons(1,:)(end);
-%     sMatrix = [n n];
+    sMatrix = zeros(max(max(tetrahedrons)));
+    for row = 1:size(tetrahedrons, 1)
+        tetrahedron = tetrahedrons(row, :);
+        S = tetrahedron2matrix(tetrahedron, vertices_list);
+        for i = 1:4
+            for j = 1:4
+                sMatrix(tetrahedron(i), tetrahedron(j)) = ...
+                   sMatrix(tetrahedron(i), tetrahedron(j)) +  S(i,j);
+            end
+        end 
+    end
 end
 
