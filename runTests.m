a6d2ef19a73ea0821d42f8867b8885f821269e45
Henrik Hillner <henrik.hillner@aalto.fi>
2017-04-07 09:34:14 +0300

    Moves runTest to test stiffness matrix

--- runTests.m
+++ runTests.m
@@ -1,6 +1,12 @@
 import matlab.unittest.TestSuite
 
-suite = TestSuite.fromClass(?tetrahedronTest);
+% suite = TestSuite.fromClass(?tetrahedronTest);
+% result = run(suite);
+% 
+% resultTable = table(result);
+% sortrows(resultTable, 'Name')
+
+suite = TestSuite.fromClass(?stiffnessMatrixTest);
 result = run(suite);
 
 resultTable = table(result);
