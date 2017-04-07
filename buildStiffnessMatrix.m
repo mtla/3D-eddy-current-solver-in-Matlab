b3b7dfddad04fcabc7f1992ec21ab4e6ccb8a0df
Henrik Hillner <henrik.hillner@aalto.fi>
2017-04-07 09:42:58 +0300

    Creates the stiffness matrix with correct dimensions

--- buildStiffnessMatrix.m
+++ buildStiffnessMatrix.m
@@ -1,8 +1,13 @@
-function [ sMatrix ] = buildStiffnessMatrix( tetrahedrons, verticesList )
+function [ sMatrix ] = buildStiffnessMatrix( tetrahedrons, vertices_list )
 %BUILDSTIFFNESMATRIX Summary of this function goes here
 %   Detailed explanation goes here
 
-    n = tetrahedrons(1,:)(end);
-    sMatrix = n;
+    sMatrix = zeros(size(tetrahedrons, 1));
+%     for row = 1:size(tetrahedrons, 1)
+%         tetrahedron = tetrahedrons(row, :);
+%         S = tetrahedron2matrix(tetrahedron, vertices_list);
+%     end
+%     n = tetrahedrons(1,:)(end);
+%     sMatrix = [n n];
 end
 
