ea37a4f58dbbb295a57907793bffddce5b5b077f
Henrik Hillner <henrik.hillner@aalto.fi>
2017-04-13 16:47:42 +0300

    makes readMesh detect fileformats

--- readMesh.m
+++ readMesh.m
@@ -1,12 +1,17 @@
 function [ mesh ] = readMesh( source )
 %READMESH Summary of this function goes here
 %   Detailed explanation goes here
+    mesh = [];
     if (ischar(source))
-        mesh = 'string';
+        fileformat = split(source, '.');
+        fileformat = fileformat(end);
+        if (strcmp(fileformat,'obj'))
+            mesh = readObj(source);
+        elseif (strcmp(fileformat, 'txt'))
+            mesh = readTxt(source);
+        end
     elseif (ismatrix(source) && size(source,2) > 2)
-        mesh = 'matrix';
-    else
-        mesh = 'error';
+        mesh = source;
     end
 end
 
