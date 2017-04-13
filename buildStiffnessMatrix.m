2c72cd23acede16a7fc9fee05a597d5bbe777fff
Henrik Hillner <henrik.hillner@aalto.fi>
2017-04-13 11:10:17 +0300

    Adds the gradient phi of the reference tetrahedron

--- buildStiffnessMatrix.m
+++ buildStiffnessMatrix.m
@@ -3,6 +3,7 @@ function [ sMatrix ] = buildStiffnessMatrix( tetrahedrons, vertices_list )
 %   Detailed explanation goes here
 
     sMatrix = zeros(max(max(tetrahedrons)));
+    % get rid of the for loop. Matlab does not like them that much
     for row = 1:size(tetrahedrons, 1)
         tetrahedron = tetrahedrons(row, :);
         S = tetrahedron2matrix(tetrahedron, vertices_list);
--- tetrahedron2matrix.m
+++ tetrahedron2matrix.m
@@ -15,7 +15,19 @@ function [ tetrahedron_matrix ] = tetrahedron2matrix( tetrahedron, node_coordina
 %
 % Syntax: tetrahedron2matrix(tetrahedron, nodes_coordinates)
 %
-
+    % the gradient of a 3D tetrahedron
+    % imagine it like a tetrahedron with the points:
+    % P1 = [ 0 0 0 ]
+    % P2 = [ 1 0 0 ]
+    % P3 = [ 0 1 0 ]
+    % P4 = [ 0 0 1 ]
+    % If you want to know the volume of the tetrahedron and start
+    % calculating from P1, the volume increases whether you go along the
+    % x-, y- or z-axis. If you start from the point P2, the volume will
+    % only increse if you move along the x-axis. P3, only y-axis etc.
+    % This is also constant everywhere in the tetrahedron
+    gradPhi_ref = [-1 -1 -1;1 0 0; 0 1 0;0 0 1]';
+    
     tetrahedron_matrix = zeros(4,4);
     x = zeros(1,4);
     y = zeros(1,4);
@@ -33,9 +45,7 @@ function [ tetrahedron_matrix ] = tetrahedron2matrix( tetrahedron, node_coordina
             % with the help of the shapefunction
             % 
             % CURRENT SHAPE FUNCTION IS NOT THE CORRECT ONE
-            if (i ~= j)
-                tetrahedron_matrix(i,j) = (x(i)-x(j))+(y(j)-y(i))/(4*volume);
-            end
+            tetrahedron_matrix(i,j) = (x(i)-x(j))+(y(j)-y(i))/(4*volume);
         end
     end
 end
