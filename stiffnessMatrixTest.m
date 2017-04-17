82ae40ee191a32844e95a6e829751035ac6df28d
Henrik Hillner <henrik.hillner@aalto.fi>
2017-04-17 22:19:51 +0300

    Makes the stiffness matrix test like the geometry test

--- stiffnessMatrixTest.m
+++ stiffnessMatrixTest.m
@@ -1,26 +1,32 @@
 classdef stiffnessMatrixTest < matlab.unittest.TestCase
-    %STIFFNESSMATRIXTEST Summary of this class goes here
-    %   Detailed explanation goes here
+    % STIFFNESSMATRIXTEST
+    % Tests whether stiffness matrixes are constructed correctly
     
-    properties
-        tetrahedrons
-        node_list
-    end
     properties (TestParameter)
+        meshes = struct(...
+            'smallCube1', [1 1 -1;1 -1 -1;1 1 1;1 -1 1;-1 1 -1;-1 -1 -1;-1 1 1;-1 -1 1], ...
+            'smallCube2', [0 0 0;0 0 1;0 1 0;0 1 1;1 0 0;1 0 1;1 1 0;1 1 1], ...
+            'randomCube1', randi(10, 8, 3),...
+            'randomCube2', randi(10, 8, 3),...
+            'randomCube3', randi(10, 8, 3),...
+            'randomCube4', randi(10, 8, 3)...
+            );
     end
     
-    methods(TestMethodSetup)
-        function createBasicTetrahedrons(testCase)
-            testCase.tetrahedrons = [8 6 5 2;7 8 5 2;7 4 8 2;7 1 4 2;7 5 1 2;7 3 4 1];
-            testCase.node_list = [1 1 -1;1 -1 -1;1 1 1;1 -1 1;-1 1 -1;-1 -1 -1;-1 1 1;-1 -1 1];
+    methods (TestMethodSetup)
+        function MethodSetup(testCase)
+            orig = rng;
+            testCase.addTeardown(@rng, orig)
+            rng(42) % Answer to the Ultimate Question of Life, the Universe, and Everything.
         end
     end
     
     methods (Test)
-        function testDimensions(testCase)
-            actOutput = buildStiffnessMatrix(testCase.tetrahedrons, testCase.node_list);
+        function testDimensions(testCase, meshes)
+            DT = readMesh(meshes);
+            actOutput = buildStiffnessMatrix(DT.ConnectivityList, DT.Points);
             actSolution = size(actOutput);
-            expSolution = ones(1,2) * size(testCase.node_list, 1);
+            expSolution = ones(1,2) * size(DT.Points, 1);
             testCase.verifyEqual(actSolution, expSolution, ... 
                 'Return matrix should be an nxn matrix where n is the number of elements');
         end
