bd67dcaa23bbe13881a24cfd56ca78e30c120c7b
Henrik Hillner <henrik.hillner@aalto.fi>
2017-04-17 21:54:17 +0300

    Finishes the body for geometry test

--- geometryTest.m
+++ geometryTest.m
@@ -3,25 +3,26 @@ classdef geometryTest < matlab.unittest.TestCase
     % Test class for testing if program can handle different kinds of input
     % meshes
     
-%     properties
-%         mesh
-%         mesh2
-%     end
-    
     properties (ClassSetupParameter)
-        generator = {'twister','combRecursive','multFibonacci'};
-    end
-    
-    properties (MethodSetupParameter)
-        seed = {0, 123, 4294967295};
+%         generator = {'twister'};
     end
     
     properties (TestParameter)
-        meshes = struct('smallCube', [1 1 -1;1 -1 -1;1 1 1;1 -1 1;-1 1 -1;-1 -1 -1;-1 1 1;-1 -1 1], ...
-            'cubeWithDuplicates', [1 1 -1;1 -1 -1;1 1 1;1 -1 1;-1 1 -1;-1 -1 -1;1 1 1;-1 -1 1], ...
+        meshes = struct(...
+            'exampleObj', strcat(pwd,'\meshes\example_mesh_3D.obj'), ...
+            'exampleTxt', strcat(pwd,'\meshes\cube_nodes.txt'), ...
+            'smallCube', [1 1 -1;1 -1 -1;1 1 1;1 -1 1;-1 1 -1;-1 -1 -1;-1 1 1;-1 -1 1], ...
             'randomCube', randi(10, 3, 8));
-        files = struct('obj', strcat(pwd,'\meshes\example_mesh_3D.obj'), ...
-            'txt', strcat(pwd,'\meshes\cube_nodes.txt'), ...
+        duplicate_meshes = struct(...
+            'cubeWithDuplicates', [1 1 -1;1 -1 -1;1 1 1;1 -1 1;-1 1 -1;-1 -1 -1;1 1 1;-1 -1 1]);
+
+        files = struct(...
+            'obj', strcat(pwd,'\meshes\example_mesh_3D.obj'), ...
+            'txt', strcat(pwd,'\meshes\cube_nodes.txt'));
+        files_result = struct(...
+            'obj', [1 1 -1;1 -1 -1;1 1 1;1 -1 1;-1 1 -1;-1 -1 -1;-1 1 1;-1 -1 1], ...
+            'txt', [0 0 0;0 0 1;0 1 0;0 1 1;1 0 0;1 0 1;1 1 0;1 1 1]);
+        files_gibberish = struct(...
             'gibberish', 'huehue', ...
             'gibberish2', 'blaoeuchaoeurcoha', ...
             'gibberishDots', 'oeuouou.hue', ...
@@ -29,57 +30,47 @@ classdef geometryTest < matlab.unittest.TestCase
             'gibberishSlashes', 'aoeuoeuo\oeuoeu/ooeuo', ...
             'gibberishDotsSlashes', 'oecuhaoeu/oeuoceuh/oeuch.hue');
     end
-
-    methods (TestClassSetup)
-        function ClassSetup(testCase, generator)
-            orig = rng;
-            testCase.addTeardown(@rng, orig)
-            rng(0, generator)
-        end
-    end
     
     methods (TestMethodSetup)
-        function MethodSetup(testCase, seed)
+        function MethodSetup(testCase)
             orig = rng;
             testCase.addTeardown(@rng, orig)
-            rng(seed)
+            rng(42) % Answer to the Ultimate Question of Life, the Universe, and Everything.
         end
     end
-    
-%     methods(TestMethodSetup)
-%         function createBasicTetrahedron(testCase)
-%             testCase.mesh = [1 1 -1;1 -1 -1;1 1 1;1 -1 1;-1 1 -1;-1 -1 -1;1 1 1;-1 -1 1];
-%             testCase.mesh2= [1 1 -1;1 -1 -1;1 1 1;1 -1 1;-1 1 -1;-1 -1 -1;-1 1 1;-1 -1 1];
-%         end
-%     end
     
     methods (Test)
         % this test gives duplicate rows on purpose to the readMesh
         % function
-        function testDuplicates(testCase, meshes)
-            actSolution = readMesh(meshes);
-            actSolution = actSolution.Points;
-            expSolution = unique(actSolution, 'rows', 'stable');
-            testCase.verifyEqual(actSolution, expSolution, ... 
-                "Mesh can't have duplicate vertices");
+        function testDuplicates(testCase, duplicate_meshes)
+            testCase.verifyWarning(@() readMesh(duplicate_meshes), ...
+                'MATLAB:delaunayTriangulation:DupPtsWarnId', ...
+                'Delaunay triangulation should raise an warning when introducing duplicate values!');
         end
-        function testTxt(testCase, files)
-            actSolution = readMesh(files);
-            expSolution = [0 0 0;0 0 1;0 1 0;0 1 1;1 0 0;1 0 1;1 1 0;1 1 1];
+        function testFalseFilenames(testCase, files_gibberish)
+            testCase.verifyError(@() readMesh(files_gibberish), ...
+                'MATLAB:FileIO:InvalidFid', ...
+                "Invalid filenames should raise an error!");
+        end
+        function testObjCube(testCase)
+            actSolution = readMesh(strcat(pwd,'\meshes\example_mesh_3D.obj'));
+            expSolution = [1 1 -1;1 -1 -1;1 1 1;1 -1 1;-1 1 -1;-1 -1 -1;-1 1 1;-1 -1 1];
             testCase.verifyEqual(actSolution.Points, expSolution, ... 
-                ".txt input failed!");
+                ".obj input failed!");
         end
-%         function testObj(testCase)
-%             actSolution = readMesh(strcat(pwd,'\meshes\example_mesh_3D.obj'));
-%             expSolution = testCase.mesh2;
-%             testCase.verifyEqual(actSolution.Points, expSolution, ... 
-%                 ".obj input failed!");
-%         end
         function testMatrix(testCase)
             actSolution = readMesh([1 1 -1;1 -1 -1;1 1 1;1 -1 1;-1 1 -1;-1 -1 -1;-1 1 1;-1 -1 1]);
             expSolution = [1 1 -1;1 -1 -1;1 1 1;1 -1 1;-1 1 -1;-1 -1 -1;-1 1 1;-1 -1 1];
             testCase.verifyEqual(actSolution.Points, expSolution, ... 
                 "Input of matrix/array failed!");
+        end
+    end
+    
+    methods (Test, ParameterCombination='sequential')
+        function testLegitFilenames(testCase, files, files_result)
+            actSolution = readMesh(files);
+            testCase.verifyEqual(actSolution.Points, files_result, ... 
+                "Input of example file failed!");
         end
     end
 end
