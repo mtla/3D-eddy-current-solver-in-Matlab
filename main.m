36d973c1e5c3d42e0c2b2fb2619e6b52566b4dad
Henrik Hillner <henrik.hillner@aalto.fi>
2017-04-17 23:38:27 +0300

    Modifies main function to include stiffness matrix calculation

--- main.m
+++ main.m
@@ -1,9 +1,4 @@
 DT = readMesh(strcat(pwd,'\meshes\example_mesh_3D.obj'));
 tetramesh(DT); % plot mesh
 
-tetrahedrons = DT.ConnectivityList;
-tetrahedron = tetrahedrons(1,:);
-points_list = DT.Points;
-
-% test tetrahedron2matrix
-tetrahedron2matrix(tetrahedron, points_list)
+buildStiffnessMatrix(DT)
