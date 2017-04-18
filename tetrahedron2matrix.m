755757b4881bfeabfd2e59b01ee8281f70db0d2f
Henrik Hillner <henrik.hillner@aalto.fi>
2017-04-18 10:41:13 +0300

    Also removes the map2global from its previous location

--- tetrahedron2matrix.m
+++ tetrahedron2matrix.m
@@ -54,19 +54,3 @@ function [ S_local ] = tetrahedron2matrix( tetrahedron, node_coordinates )
     S_local = w1 * S_local * abs(det(B));
 end
 
-function [ B, b ] = map2global( tetrahedron, node_coordinates ) 
-% Maps a single tetrahedron to the global unit tetrahedron so that
-% g_global = B*x_ref + b
-
-    % build the tetrahedron matrix so that
-    % matrix = [x_1...x_4 ; y_1...y_4 ; z_1...z_4]
-    m_tetrahedron = zeros(3,4);
-    for n = 1:4
-        m_tetrahedron(1,n) = node_coordinates(tetrahedron(n),1);
-        m_tetrahedron(2,n) = node_coordinates(tetrahedron(n),2);
-        m_tetrahedron(3,n) = node_coordinates(tetrahedron(n),3);
-    end
-    B = [m_tetrahedron(:,2)-m_tetrahedron(:,1) m_tetrahedron(:,3)-m_tetrahedron(:,1) m_tetrahedron(:,4)-m_tetrahedron(:,1)];
-    b = m_tetrahedron(:,1);
-end
-
