49651970fea325787e0f6cdf12faad2b8efabe1f
Henrik Hillner <henrik.hillner@aalto.fi>
2017-04-21 12:05:25 +0300

    Adds dirichlet nodes to main

--- main.m
+++ main.m
@@ -1,4 +1,5 @@
 DT = readMesh(strcat(pwd,'\meshes\example_mesh_3D.obj'));
+dirichletNodes = readDirichletNodes(strcat(pwd,'\meshes\example_mesh_3D_dirichlet.obj'), DT);
 tetramesh(DT); % plot mesh
 
-buildStiffnessMatrix(DT)
+% buildStiffnessMatrix(DT)
