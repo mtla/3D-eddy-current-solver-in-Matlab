0fc887d2c3aa706e08900a7a511ff2d130b8c26b
Henrik Hillner <henrik.hillner@aalto.fi>
2017-04-28 16:56:35 +0300

    fixes typo in main

--- main.m
+++ main.m
@@ -1,18 +1,17 @@
-DT = readMesh(strcat(pwd,'\meshes\long_bar.obj'));
-dirichletNodes = readDirichletNodes(strcat(pwd,'\meshes\example_mesh_3D_dirichlet.obj'), DT);
+DT = readMesh(strcat(pwd,'\meshes\long bar.obj'));
+dirichletNodes = readDirichletNodes(strcat(pwd,'\meshes\long_bar_dirichlet.obj'), DT);
 tetramesh(DT); % plot mesh
 
 %reluctivity of each element [A/(Tm)]
 reluctivity = (pi*4e-7);
 np = size(DT.Points,1);
 %current density in each element [A/m^2]
-currentDensity = ones(np,1); % last element has a source current [ 0 1 0 1 0 1]
+currentDensity = ones(np,1); % last element has a source current
 currentDensity(dirichletNodes) = 0;
 %dirichletNodes;
 
 S = buildStiffnessMatrix(DT, reluctivity);
 f = buildLoadVector(DT, currentDensity);
-
 
 freeNodes = setdiff(1:np, DirichletNodes); %nodes NOT in Dirichlet bnd
 
@@ -22,7 +21,7 @@ Afree = S(freeNodes,freeNodes) \ f(freeNodes);
 
 %assembling solution in the entire region
 A_total = zeros(Np,1);
-A_total(freeNodes) = Afree;
+A_total(freeNodes) = Afree
 
 figure
 scatter3(DT.Points(:,1),DT.Points(:,2),DT.Points(:,3),abs(A_total)/100+1);
