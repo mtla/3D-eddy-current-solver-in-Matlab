c6f8b5292cd355ef9325ebc97bb402bf9906309e
Henrik Hillner <henrik.hillner@aalto.fi>
2017-04-28 12:02:47 +0300

    Updates main to include load vector and omega calculation

--- main.m
+++ main.m
@@ -2,4 +2,7 @@ DT = readMesh(strcat(pwd,'\meshes\example_mesh_3D.obj'));
 dirichletNodes = readDirichletNodes(strcat(pwd,'\meshes\example_mesh_3D_dirichlet.obj'), DT);
 tetramesh(DT); % plot mesh
 
-% buildStiffnessMatrix(DT)
+S = buildStiffnessMatrix(DT)
+f = buildLoadVector(DT)
+
+Omega = f * s^-1
