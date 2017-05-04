3bf97fe4942c243589a7f5dbbf0d165dd7417a00
Henrik Hillner <hhillner@gmail.com>
2017-05-05 00:36:50 +0300

    converts tetrahedron from points to edges

--- msh.m
+++ msh.m
@@ -16,9 +16,32 @@ classdef msh
             obj.Points = obj.DT.Points;
             obj.TetrahedronsByPoints = obj.DT.ConnectivityList;
             obj.Edges = edges(obj.DT);
+            obj.TetrahedronsByEdges = tetrahedrons2edges(obj);
         end
         function tetramesh(obj)
             tetramesh(obj.DT)
+        end
+        function p = points(obj)
+            p = obj.Points;
+        end
+        function e = edges(obj)
+            e = obj.Edges;
+        end
+%         function s = size(obj)
+%             s = [size(obj.Points); size(obj.Edges); size(obj.TetrahedronsByPoints)];
+%         end
+    end
+    methods(Access = private)
+        function tbe = tetrahedrons2edges(obj)
+            tbe = [];%zeros(size(obj.Edges,1));
+            for row = 1:size(obj.TetrahedronsByPoints, 1)
+                tetrahedron = obj.TetrahedronsByPoints(row,:);
+                pairs = combnk(tetrahedron,2);
+                [~,indexes] = ismember(pairs, obj.Edges, 'rows');
+                [~,tmp] = ismember(pairs, fliplr(obj.Edges), 'rows');
+                indexes = indexes + tmp;
+                tbe = [tbe; indexes'];
+            end
         end
     end
     
