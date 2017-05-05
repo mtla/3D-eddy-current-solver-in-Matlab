8778dd62daa763469efe24a1731fd2038f5b6c66
Henrik Hillner <henrik.hillner@aalto.fi>
2017-05-05 11:00:17 +0300

    Adds output file dialog to simulate

--- simulate.m
+++ simulate.m
@@ -1,11 +1,11 @@
-% [filename, filefolder] = uigetfile({'*.obj','Object Files';'*.txt','Raw Text File'}, 'Read obj-file');
-% mesh_path = strcat(filefolder,filename);
-% 
-% [filename, filefolder] = uigetfile({'*.obj','Object Files';'*.txt','Raw Text File'}, 'Read obj-file');
-% dirichlet_path = strcat(filefolder,filename);
-% 
-% msh = readMesh(mesh_path);
-% dirichletNodes = readDirichletNodes(msh, dirichlet_path);
+[filename, filefolder] = uigetfile({'*.obj','Object Files';'*.txt','Raw Text File'}, 'Read obj-file');
+mesh_path = strcat(filefolder,filename);
+
+[filename, filefolder] = uigetfile({'*.obj','Object Files';'*.txt','Raw Text File'}, 'Read obj-file');
+dirichlet_path = strcat(filefolder,filename);
+
+msh = readMesh(mesh_path);
+dirichletNodes = readDirichletNodes(msh, dirichlet_path);
 
 prompt = {'Permittivity in element:','Permeability in element:'};
 dlg_title = 'Input';
@@ -17,3 +17,6 @@ constants = inputdlg(prompt,dlg_title,num_lines,defaultans);
 % plot mesh
 figure(1)
 tetramesh(msh);
+
+[filename, filefolder] = uisavefile({'*.csv','Comma separated file';'*.txt','Raw Text File';'*.vtk','Binary Paraview file'}, 'Save output');
+output_path = strcat(filefolder,filename);
